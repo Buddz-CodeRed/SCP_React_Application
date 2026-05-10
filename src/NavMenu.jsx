bce86400564c0f4496dda31a481080e134d6637d
Buddz-CodeRed <badi9code.red@gmail.com>
2026-05-11 11:36:34 +1200

    add NavMenu component with initial imports and structure

---
+++ src/NavMenu.jsx
@@ -0,0 +1,11 @@
+import {useEffect, useState} from 'react' // useEffect: fetching data | useState: storing data
+import {Link} from 'react-router-dom' // navigate through links with out full page reloads
+import {supabase} from './supabase' // connects to database
+
+export default function NavMenu() {
+
+    
+
+
+}
+
