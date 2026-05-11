129fc7520aa52b68afa46216dd74979ac2779f48
Buddz-CodeRed <badi9code.red@gmail.com>
2026-05-11 12:02:31 +1200

    fetch records from supabase and update state in NavMenu component

--- src/NavMenu.jsx
+++ src/NavMenu.jsx
@@ -4,8 +4,30 @@ import {supabase} from './supabase' // connects to database
 
 export default function NavMenu() {
 
-    
+    // Store list of records starting at an empty (array) state !!!
+    // Replace empty array with fetched data when function is called
+    const [records, setRecord] = useState([])
 
+    useEffect(
+        () => {
+            // define async function
+            const fetchRecords = async () => {
+                // query db only selecting the id and item and store in 'data' variable
+                // waits for process to complete before continuing
+                const {data, error} = await supabase.from('scp_data').select('id', 'item') 
+                if (error)
+                {
+                    console.error(error) // display error
+                }
+                else
+                {
+                    // update records state with array of records; triggers re-render
+                    setRecord(data)
+                }
+            }
+            fetchRecords()
+        }, []
+    )
 
 }
 
