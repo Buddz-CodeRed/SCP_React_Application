67ab3ec39b71e03d1c0dee326d27e2c935834051
Buddz-CodeRed <badi9code.red@gmail.com>
2026-05-11 15:42:22 +1200

    refactor App component to include NavMenu and Routes for RecordDetails and AdminPanel

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import NavMenu from "./NavMenu"
+import RecordDetails from "./RecordDetails"
+import AdminPanel from "./AdminPanel"
 
 export default function App() {
   return(
     <Router>
-      
+      <NavMenu/>
+      <Routes>
+        <Route path='/' element=
+        {
+          // Design Main App Dashboard
+          <div>
+            <h1>Testing React Application</h1>
+          </div>
+        }>
+        </Route>
+        <Route path='/records/:id'>{<RecordDetails/>}</Route>
+        <Route path='/admin/'>{<AdminPanel/>}</Route>
+      </Routes>
     </Router>
   )
 }
